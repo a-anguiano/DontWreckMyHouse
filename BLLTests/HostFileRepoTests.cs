b959a5e1e54162cfb370c21957f7dc3bf83ec735
Allison Anguiano <aanguiano@dev-10.com>
2022-04-07 10:39:02 -0500

    DAL hostRepo test passed

--- BLLTests/HostFileRepoTests.cs
+++ BLLTests/HostFileRepoTests.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using NUnit.Framework;
+using DontWreckMyHouse.Core;
 
 namespace DontWreckMyHouse.DAL.Tests
 {
-    public class HostFileRepoTests  //public?
+    public class HostFileRepoTests
     {
-        
+        const string SEED_DIRECTORY = "TestData";
+        const string SEED_FILE = "TestHostSeed.csv";
+        const string TEST_DIRECTORY = "Test";
+        const string TEST_FILE = "hosts.csv";
+
+        string SEED_PATH = Path.Combine(SEED_DIRECTORY, SEED_FILE);
+        string TEST_PATH = Path.Combine(TEST_DIRECTORY, TEST_FILE);
+
+        const int HOST_TX_COUNT = 17;      //count
+        const string HOST_KY_NAME = "KY";
+
+
+        private HostFileRepo _repo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //if (File.Exists(LogFile)) File.Delete(LogFile);
+            if (!Directory.Exists(TEST_DIRECTORY))
+            {
+                Directory.CreateDirectory(TEST_DIRECTORY);
+            }
+
+            //if (File.Exists(TEST_FILE)) File.Delete(TEST_FILE);
+
+            File.Copy(SEED_PATH, TEST_PATH, true);
+
+            _repo = new HostFileRepo(TEST_PATH);       //test path
+        }
+
+        [Test]
+        public void ShouldFindByState()
+        {
+            List<Host> hosts = _repo.FindByState(HOST_KY_NAME);
+            Assert.AreEqual(HOST_TX_COUNT, hosts.Count);
+        }
     }
 }
--- DontWreckMyHouse.DAL/HostFileRepo.cs
+++ DontWreckMyHouse.DAL/HostFileRepo.cs
@@ -7,6 +7,7 @@ using DontWreckMyHouse.Core.Interfaces;
 using DontWreckMyHouse.Core.Models;
 using DontWreckMyHouse.Core;
 using System.IO;
+using DontWreckMyHouse.Core.Exceptions;
 
 namespace DontWreckMyHouse.DAL
 {
@@ -20,10 +21,56 @@ namespace DontWreckMyHouse.DAL
         }
         public List<Host> FindByState(string stateAbbr)
         {
-            throw new NotImplementedException();
+            var hosts = new List<Host>();
+
+            //if (!File.Exists(path))
+            //{
+            //    return _reservations;           //return type....
+            //}
+
+            string[] lines = null;
+            try
+            {
+                lines = File.ReadAllLines("hosts.csv");
+            }
+            catch (IOException ex)
+            {
+                throw new RepoExceptions("Could not read hosts.", ex);
+            }
+
+            for (int i = 1; i < lines.Length; i++) // skip the header
+            {
+                string[] fields = lines[i].Split(",", StringSplitOptions.TrimEntries);
+                Host host = Deserialize(fields);
+                if (host != null && host.State == stateAbbr)
+                {
+                    hosts.Add(host);
+                }
+            }
+            return hosts;
         }
 
         //Deserialize
+        private Host Deserialize(string[] fields)
+        {
+            if (fields.Length != 10)
+            {
+                return null;
+            }
+
+            Host result = new Host();
+            result.Id = fields[0];          //guid
+            result.LastName = fields[1];
+            result.Email = fields[2];
+            result.Phone = fields[3];
+            result.Address = fields[4];
+            result.City = fields[5];
+            result.State = fields[6];
+            result.Zip = int.Parse(fields[7]);
+            result.StandardRate = decimal.Parse(fields[8]);
+            result.WeekendRate = decimal.Parse(fields[9]);
+            return result;
+        }
         //NOT ADDING SO NO NEED TO WRITE OR SERIALIZE
     }
 }
